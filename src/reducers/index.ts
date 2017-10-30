ce244cb6ee9fc0d96c252e1d092302f075b01519
yvanwangl <1012305328@qq.com>
2017-10-30 22:47:26 +0800

    modify create store

---
+++ src/reducers/index.ts
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import hello from './hello';
+
+let rootReducer = combineReducers({
+    hello
+});
+
+export default rootReducer;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,7 @@
+import {StoreState} from './types';
+import rootReducer from '../reducers/index';
 import {createStore} from 'redux';
+
+export default function configStore(initialState: StoreState){
+    return createStore(rootReducer, initialState);
+}
